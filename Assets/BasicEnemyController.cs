0153b13d5f514700d7fc7b2ab1aaf6aa807626db
Unknown <ianouellette47@gmail.com>
2017-02-02 11:38:23 -0500

    Added Instantiators, Scripts, and Path
    Added instantiators for basic enemies and towers, enemies can be spawned with spacebar and towers with m1. Enemies follow the path, towers cannot be placed on paths or on top of one another.

---
+++ Assets/BasicEnemyController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicEnemyController : MonoBehaviour {
+
+    GameManager manager;
+    public Vector3 velocity;
+    int turn = 0;
+
+	// Use this for initialization
+	void Start () {
+        velocity = new Vector3(0, -.02f, 0);
+        manager = GameObject.FindObjectOfType<GameManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 where = transform.position;
+        where += velocity;
+        transform.position = where;
+
+        if(turn == 0 && transform.position.y <= 2.4)
+        {
+            turn = 1;
+            velocity = new Vector3(.02f, 0, 0);
+        }
+        else if (turn == 1 && transform.position.x >= 2.3)
+        {
+            turn = 2;
+            velocity = new Vector3(0, -.02f, 0);
+        }
+        else if (turn == 2 && transform.position.y <= -0.15)
+        {
+            turn = 3;
+            velocity = new Vector3(-.02f, 0, 0);
+        }
+        else if (turn == 3 && transform.position.x <= -2.65)
+        {
+            turn = 4;
+            velocity = new Vector3(0, -.02f, 0);
+        }
+        else if (turn == 4 && transform.position.y <= -2.85)
+        {
+            turn = 5;
+            velocity = new Vector3(.02f, 0, 0);
+        }
+
+
+        if (transform.position.x > 11.5 && turn == 5)
+        {
+            manager.removeEnemy(this);
+        }
+
+    }
+
+    public bool breakthrough()
+    {
+        if (transform.position.x > 11.5 && turn == 5)
+        {
+            manager.removeEnemy(this);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
---
+++ Assets/GameManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject BasicEnemy;
+    public List<BasicEnemyController> BasicEnemies;
+
+    public GameObject Tower;
+    public List<TowerController> Towers;
+
+    public LayerMask pathMask;
+    public LayerMask towerMask;
+    
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(Input.GetKeyDown(KeyCode.Space)) //Test spawns enemies
+        {
+            Object obj = Instantiate(BasicEnemy.gameObject, new Vector3(-6.7f, 6.21f, 0), Quaternion.identity);
+            BasicEnemyController e = ((GameObject)obj).GetComponent<BasicEnemyController>();
+            BasicEnemies.Add(e);
+        }
+
+        if(Input.GetMouseButtonDown(0)) //Creates towers
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            bool pathCheck = Physics.Raycast(new Ray(mousePos, Vector3.forward), 10, pathMask);
+            bool towerCheck = Physics2D.Raycast(mouseRay.origin, mouseRay.direction, 10, towerMask);
+            if (!pathCheck && !towerCheck)
+            {
+                Object obj = Instantiate(Tower.gameObject, new Vector3(mousePos.x, mousePos.y, 0), Quaternion.identity);
+                TowerController t = ((GameObject)obj).GetComponent<TowerController>();
+                Towers.Add(t);
+            }
+        }
+
+        /*foreach (BasicEnemyController b in BasicEnemies)
+        {
+            if(b.breakthrough()) //Checks if any enemies reached the end
+            {
+                
+                print("DELETED");
+            }
+        }*/
+	}
+
+    public void removeEnemy(BasicEnemyController b)
+    {
+        BasicEnemies.Remove(b);
+        Destroy(b.gameObject);
+    }
+    
+}
